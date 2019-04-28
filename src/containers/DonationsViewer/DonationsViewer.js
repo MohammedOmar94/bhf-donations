6b7bf7dee2ae33211a9882c6ccae7f09965121a3
Heights94 <mohammed_omar94@hotmail.co.uk>
2019-04-28 15:20:34 +0100

    Added some styling to DonationsViewer

--- src/containers/DonationsViewer/DonationsViewer.js
+++ src/containers/DonationsViewer/DonationsViewer.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 import axios from "axios";
+import classes from './DonationsViewer.module.css';
 import Donations from '../../components/Donations/Donations';
 
 class DonationsViewer extends Component {
@@ -41,13 +42,21 @@ class DonationsViewer extends Component {
   render() {
     const charityInfo = this.state.charityInfo;
     const donations = this.state.donations;
+    let themeColour = [];
+    if (charityInfo.themeColour) {
+      themeColour = Object.values(charityInfo.themeColour);
+      // Reorder the array so it's in the rgba format.
+      [themeColour[0], themeColour[1], themeColour[2], themeColour[3]] = [themeColour[1], themeColour[2], themeColour[3], themeColour[0]];
+    }
     return (
-      <section>
-        <img src={charityInfo.logoAbsoluteUrl} alt='BHF Logo' width='50' height='50' />
-        <h2>{charityInfo.name}</h2>
-        <p>{charityInfo.description}</p>
+      <Fragment>
+        <header className={classes.CharityInformation}>
+          <img src={charityInfo.logoAbsoluteUrl} alt='BHF Logo' />
+          <h2 style={{ color: `rgba(${themeColour}` }}>{charityInfo.name}</h2>
+          <p>{charityInfo.description}</p>
+        </header>
         <Donations donations={donations} />
-      </section>
+      </Fragment>
     );
   }
 }
