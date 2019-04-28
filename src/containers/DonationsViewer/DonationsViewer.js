2b5197f5a27b945c28dd00b9e02f51dbd30a9549
Heights94 <mohammed_omar94@hotmail.co.uk>
2019-04-28 11:53:58 +0100

    Retrieves BHF info and displays some of the data in DonationsViewer

--- src/containers/DonationsViewer/DonationsViewer.js
+++ src/containers/DonationsViewer/DonationsViewer.js
@@ -1,11 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import axios from "axios";
 
 class DonationsViewer extends Component {
+  state = {
+    charityInfo: {}
+  };
+
+  componentDidMount() {
+    axios
+      .get("/charity/183092", {
+        headers: {
+          "Content-Type": "application/json"
+        },
+        data: {}
+      })
+      .then(response => this.setState({ charityInfo: response.data }))
+      .catch(error => console.log(error));
+  }
+
   render() {
+    const charityInfo = this.state.charityInfo;
+    console.log(charityInfo);
     return (
-      <div>
-        View Donations Here
-      </div>
+      <section>
+        <img src={charityInfo.logoAbsoluteUrl} alt='BHF Logo' width='50' height='50' />
+        <h2>{charityInfo.name}</h2>
+        <p>{charityInfo.description}</p>
+      </section>
     );
   }
 }
