fb28205267e4029d2bdb9dc5d6ea38efe6bf9943
Heights94 <mohammed_omar94@hotmail.co.uk>
2019-04-28 16:21:04 +0100

    Added some styling to the Donations component

--- src/components/Donations/Donations.js
+++ src/components/Donations/Donations.js
@@ -1,25 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import classes from './Donations.module.css';
+
 const donations = props => {
   const donationsList = props.donations.map((donation, index) => {
     const donationDate = new Date(parseInt(donation.donationDate.substr(6))).toLocaleDateString("en-GB");
     return (
-      <section key={donation.donorDisplayName + index}>
-        <p>{donationDate}</p>
-        <img src={donation.imageUrl} alt="BHF User" width="50" height="50" />
-        <p>
-          <b>{donation.donorDisplayName}</b> donated{" "}
-          <b>
-            {donation.donorLocalAmount.toFixed(2)}{" "}
-            {donation.donorLocalCurrencyCode}
-          </b>
-        </p>
-        <p>{donation.message}</p>
+      <section key={donation.donorDisplayName + index} className={classes.Donation}>
+        <img src={donation.imageUrl} alt="BHF User" />
+        <section>
+          <p>{donationDate}</p>
+          <p className={classes.DonorName}>
+            <b>{donation.donorDisplayName}</b> donated{" "}
+            <b>
+              {donation.donorLocalAmount.toFixed(2)}{" "}
+              {donation.donorLocalCurrencyCode}
+            </b>
+          </p>
+          <p>{donation.message}</p>
+        </section>
       </section>
     );
   });
-  return <article>{donationsList}</article>;
+  return <article className={classes.Donations}>{donationsList}</article>;
 };
 
 donations.propTypes = {
